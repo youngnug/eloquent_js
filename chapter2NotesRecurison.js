9bba47b6403f8a3d15e47e9d5ca1b0252d254f9d
Brandon corey <93304067+youngnug@users.noreply.github.com>
2022-07-06 17:15:07 -0400

    Update chapter2NotesRecurison.js

--- chapter2NotesRecurison.js
+++ chapter2NotesRecurison.js
@@ -3,14 +3,15 @@ function power(base, exponent) {
     return 1;
   } else {
     return base * power(base, exponent - 1);
-    //Recurses down to base case
-    //		2	*		2		1-1 = 0 --> 2 * (2^0) --> 1
-    //Base case returns 1
-    //		2	*		2		2-1 = 1 --> 2 * (2^1) --> 2
-    //		2	*		2		3-1 = 2 --> 2 * (2^1) --> 4
-    //		2	*		2					2 * (2^3) --> 8
+    // Base case returns 1                  power(2 * (0))   --> 1
+    // ^                                    |
+    // |                                    |
+    // Recurses down to base case           v
+    // ^                                    power(2 * (2^0)) --> 2
+    // |                                    power(2 * (2^1)) --> 4
+    // Pass in (2,3) (See logic below)-->   power(2 * (2^2)) --> 8
   }
-  //Remember, you're returning exponent - 1 as the value of exponent when its called
+  //Remember, you're returning exponent - 1 as the value of exponent when its called.
   //Logic: Pass in 2, 3 to power function. Well 2^3 is equal to 2 * 2^2 and so on until you hit base case.
   
 }
